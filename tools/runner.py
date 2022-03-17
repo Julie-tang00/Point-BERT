36772a96cbd13d16aa0829b82afadab36fd07e4b
Xumin Yu <1090414006@qq.com>
2022-03-17 19:45:46 +0800

    Update runner.py

--- tools/runner.py
+++ tools/runner.py
@@ -143,7 +143,7 @@ def run_net(args, config, train_writer=None, val_writer=None):
 
             if args.distributed:
                 loss_1 = dist_utils.reduce_tensor(loss_1, args)
-                loss_1 = dist_utils.reduce_tensor(loss_2, args)
+                loss_2 = dist_utils.reduce_tensor(loss_2, args)
                 losses.update([loss_1.item() * 1000, loss_2.item() * 1000])
             else:
                 losses.update([loss_1.item() * 1000, loss_2.item() * 1000])
