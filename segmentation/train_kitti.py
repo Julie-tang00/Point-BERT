e3df01cb36cc2e6d82960cc1291b9c45663acb7a
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-03-19 13:14:23 -0400

    fix imports

--- segmentation/train_kitti.py
+++ segmentation/train_kitti.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 import torch.utils.data as data
 from tqdm import tqdm
 from constants import ROOT_DIR
-import models.PointTransformer
+import models.PointTransformer as PointTransformer
 from timm.scheduler import CosineLRScheduler
 import os
 import numpy as np
